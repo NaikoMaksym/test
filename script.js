f41820f80dd1e1c0c516485a5e2a68939e2e1326
Maksym <Najmaks@ukr.net>
2018-12-09 18:47:58 +0200

    Это мой первый коммит

---
+++ script.js
@@ -0,0 +1,19 @@
+' use strict ';
+var leftBorderWidth = 1;
+let second  = 10;
+const pi = 3.14;
+console.log(leftBorderWidth);
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(persone["name"]);
+let arr = ['plam.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[0]);
+//alert('Hallo World!');
+//let answer = confirm("Are you here?");
+//console.log(answer);
+//let answer = prompt("Ксть ли Вам 18 лет", "Да");
+//console.log(answer);
+console.log(typeof(null));
